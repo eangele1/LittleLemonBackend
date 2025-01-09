75478997385f3f670b0635d9ae750a07c485b7d5
eangele1 <ezerangeles1@gmail.com>
2025-01-09 15:10:00 -0800

    Added view security

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -9,14 +9,18 @@ from .serializers import MenuSerializer
 from rest_framework.viewsets import ModelViewSet
 from .models import Booking
 from .serializers import BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
